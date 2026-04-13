ec77c86da399141c2b433e82a89c67d0d5f4774e
Troll338cz <59578676+Troll338cz@users.noreply.github.com>
2026-04-13 10:22:18 +0200

    Add few comments to main file

--- payloads/firewall.sh
+++ payloads/firewall.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 # APN 1
 iptables -A  INPUT -i rmnet_data0 -p tcp -m tcp --dport 23 -j DROP
 iptables -A  INPUT -i rmnet_data0 -p tcp -m tcp --dport 80 -j DROP
 iptables -A  INPUT -i rmnet_data0 -p tcp -m tcp --dport 443 -j DROP
 iptables -A  INPUT -i rmnet_data0 -p tcp -m tcp --dport 2222 -j DROP
+iptables -A  INPUT -i rmnet_data0 -p tcp -m tcp --dport 6969 -j DROP
 iptables -A  INPUT -i rmnet_data0 -p udp -m udp --dport 10001 -j DROP
 ip6tables -A  INPUT -i rmnet_data0 -p tcp -m tcp --dport 23 -j DROP
 ip6tables -A  INPUT -i rmnet_data0 -p tcp -m tcp --dport 80 -j DROP
@@ -15,6 +17,7 @@ iptables -A  INPUT -i rmnet_data1 -p tcp -m tcp --dport 23 -j DROP
 iptables -A  INPUT -i rmnet_data1 -p tcp -m tcp --dport 80 -j DROP
 iptables -A  INPUT -i rmnet_data1 -p tcp -m tcp --dport 443 -j DROP
 iptables -A  INPUT -i rmnet_data1 -p tcp -m tcp --dport 2222 -j DROP
+iptables -A  INPUT -i rmnet_data1 -p tcp -m tcp --dport 6969 -j DROP
 iptables -A  INPUT -i rmnet_data1 -p udp -m udp --dport 10001 -j DROP
 ip6tables -A  INPUT -i rmnet_data1 -p tcp -m tcp --dport 23 -j DROP
 ip6tables -A  INPUT -i rmnet_data1 -p tcp -m tcp --dport 80 -j DROP
@@ -26,6 +29,7 @@ iptables -A  INPUT -i rmnet_data2 -p tcp -m tcp --dport 23 -j DROP
 iptables -A  INPUT -i rmnet_data2 -p tcp -m tcp --dport 80 -j DROP
 iptables -A  INPUT -i rmnet_data2 -p tcp -m tcp --dport 443 -j DROP
 iptables -A  INPUT -i rmnet_data2 -p tcp -m tcp --dport 2222 -j DROP
+iptables -A  INPUT -i rmnet_data2 -p tcp -m tcp --dport 6969 -j DROP
 iptables -A  INPUT -i rmnet_data2 -p udp -m udp --dport 10001 -j DROP
 ip6tables -A  INPUT -i rmnet_data2 -p tcp -m tcp --dport 23 -j DROP
 ip6tables -A  INPUT -i rmnet_data2 -p tcp -m tcp --dport 80 -j DROP
@@ -37,10 +41,12 @@ iptables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 23 -j DROP
 iptables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 80 -j DROP
 iptables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 443 -j DROP
 iptables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 2222 -j DROP
+iptables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 6969 -j DROP
 iptables -A  INPUT -i rmnet_data3 -p udp -m udp --dport 10001 -j DROP
 ip6tables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 23 -j DROP
 ip6tables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 80 -j DROP
 ip6tables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 443 -j DROP
 ip6tables -A  INPUT -i rmnet_data3 -p tcp -m tcp --dport 2222 -j DROP
 ip6tables -A  INPUT -i rmnet_data3 -p udp -m udp --dport 10001 -j DROP
+
 exit 0
